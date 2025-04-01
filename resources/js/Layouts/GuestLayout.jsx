aeb276c0569b59299e7293eaa4499d500100c6c3
UvvUmi <formaldaniil@gmail.com>
2025-04-01 03:40:45 +0300

    initial commit

---
+++ resources/js/Layouts/GuestLayout.jsx
@@ -0,0 +1,19 @@
+import { Link } from '@inertiajs/react';
+import { Atom }  from '@/Components/Atom.tsx';
+
+export default function GuestLayout({ children }) {
+    return (
+        <div className="flex min-h-screen flex-col items-center bg-gray-100 dark:bg-gradient-to-br from-[#0f172a]  to-[#334155] pt-6 sm:justify-center sm:pt-0">
+            
+            <div className="flex flex-row justify-center items-center space-x-4">
+                <Link href="/">
+                    <Atom className='h-[120px] w-[120px]'/>
+                </Link>
+            </div>
+
+            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+                {children}
+            </div>
+        </div>
+    );
+}
