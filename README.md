9ba3f4f6fef9c3165692f22bab1f1ed8091d60bb
UvvUmi <formaldaniil@gmail.com>
2025-04-06 05:34:49 +0300

    readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Search by keywords
-User upload and use avatar or set default(gravatar or something)
-Customize cards
-Specific Threads with topic, images, content and comments
-User Create thread/comments
-Threads/comments editable, removable only by author
+-Search by keywords
+-User upload and use avatar or set default(gravatar or something)
+-Customize cards
+-Specific Threads with topic, images, content and comments
+-User Create thread/comments
+-Threads/comments editable, removable only by author
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -25,7 +25,7 @@ export default function AuthenticatedLayout({ header, children }) {
                                 <Atom className="block h-12 w-auto" />
                             </div>
                             <div className='text-white font-bold ms-3 items-center flex cursor-pointer'><a href={route('dashboard')}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
                                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
                                 <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/></svg></a>
                             </div>
@@ -65,6 +65,10 @@ export default function AuthenticatedLayout({ header, children }) {
                                             href={route('profile.edit')}
                                         >
                                             {Cookies.get("language") === "lt" ? "Paskyra" : "Profile"}
+                                        </Dropdown.Link>
+                                        <Dropdown.Link
+                                            href={route('dashboard')}
+                                        >{Cookies.get("language") === "lt" ? "Pagrindinis" : "Homepage"}
                                         </Dropdown.Link>
                                         <Dropdown.Link
                                             href={route('logout')}
@@ -138,6 +142,15 @@ export default function AuthenticatedLayout({ header, children }) {
                     <div className="pb-3 border-t border-gray-200">
                         <div className="flex w-full items-start py-1 pe-4">
                             <LanguageMenu/>
+                        </div>
+                        <div className="mt-2">
+                            <ResponsiveNavLink
+                                method="get"
+                                href={route('dashboard')}
+                                as="button"
+                                className="text-white">
+                                {Cookies.get('language') === "lt" ? "Pagrindinis" : "Homepage"}
+                            </ResponsiveNavLink>
                         </div>
                         <div className="mt-2">
                             <ResponsiveNavLink
