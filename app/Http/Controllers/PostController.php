848b7c35f8dc9f5e7e73eff5e462c8c11a33d914
UvvUmi <formaldaniil@gmail.com>
2025-04-09 08:02:47 +0300

    Comment in threads

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Thread;
+use App\Models\Comment;
 
 class PostController extends Controller
 {
     public function show(string $id)
     {
         return Inertia::render('Thread', [
-            'thread' => Thread::findOrFail($id)
+            'thread' => Thread::findOrFail($id),
+            'comments' => Comment::select('*')->where('thread_id', $id)->get()->toArray(),
         ]);
     }
 }
--- resources/js/Pages/Thread.jsx
+++ resources/js/Pages/Thread.jsx
@@ -3,11 +3,18 @@ import { Head } from '@inertiajs/react';
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
 
-export default function Dashboard({thread}) {
+export default function Dashboard({thread, comments}) {
+    useEffect(() => {
+        console.log(comments);
+    }, [comments]);
     return (
         <AuthenticatedLayout>
             {thread.title}<br/>
-            {thread.id}
+            {thread.id}<br/>
+            {comments.length != 0 
+                ? comments.map(comment => (<div className='text-white'>{comment.comment}</div>))
+                : <div>No comments here yet</div>
+            }
             <Head title={thread.title} />
         </AuthenticatedLayout>
     );
